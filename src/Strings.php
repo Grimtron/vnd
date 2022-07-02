c33e60f8a8a7134663e2aa14876e4e06ec644167
Ron Nanko <ronnanko@gm-squared.de>
2022-07-02 21:52:34 +0200

    strBulkReplace

--- src/Strings.php
+++ src/Strings.php
@@ -1,40 +1,41 @@
 <?php
+
 namespace Drupal\vnd;
 
 class Strings
 {
   /**
-  * Prüft, ob eine übergebene Zeichenkette mit einer zweiten übergebenen Zeichenkette endet.
-  *
-  * @param string $haystack Zeichenkette, deren Ende geprüft werden soll.
-  * @param string $needle Zeichenkette, die am Ende von $haystack gesucht werden soll
-  * @returns true, falls $haystack mit $needle endet, sonst false
-  */
+   * Prüft, ob eine übergebene Zeichenkette mit einer zweiten übergebenen Zeichenkette endet.
+   *
+   * @param string $haystack Zeichenkette, deren Ende geprüft werden soll.
+   * @param string $needle Zeichenkette, die am Ende von $haystack gesucht werden soll
+   * @returns true, falls $haystack mit $needle endet, sonst false
+   */
   public static function EndsWith($haystack, $needle)
   {
-    return substr($haystack, -strlen($needle))===$needle;
+    return substr($haystack, -strlen($needle)) === $needle;
   }
 
   /**
-  * Prüft, ob eine übergebene Zeichenkette mit einer zweiten übergebenen Zeichenkette beginnt.
-  *
-  * @param string $haystack Zeichenkette, deren Ende geprüft werden soll.
-  * @param string $needle Zeichenkette, die am Ende von $haystack gesucht werden soll
-  * @returns true, falls $haystack mit $needle endet, sonst false
-  */
+   * Prüft, ob eine übergebene Zeichenkette mit einer zweiten übergebenen Zeichenkette beginnt.
+   *
+   * @param string $haystack Zeichenkette, deren Ende geprüft werden soll.
+   * @param string $needle Zeichenkette, die am Ende von $haystack gesucht werden soll
+   * @returns true, falls $haystack mit $needle endet, sonst false
+   */
   public static function StartsWith($haystack, $needle)
   {
     return (substr($haystack, 0, strlen($needle)) === $needle);
   }
 
   /**
-  * Kürzt einen Text auf eine angegebene Zeichenlänge. Ist der Text kürzer als die gewünschte
-  * Maximallänge, wird er unverändert zurückgegeben.
-  *
-  * @param string $text Text, der gekürzt werden soll
-  * @param int $maxLength Maximal erlaubte Länge für den Text.
-  * @param string $moreTextIndicator Zeichenkette, die an das Ende eines gekürztes Textes gehängt werden soll
-  */
+   * Kürzt einen Text auf eine angegebene Zeichenlänge. Ist der Text kürzer als die gewünschte
+   * Maximallänge, wird er unverändert zurückgegeben.
+   *
+   * @param string $text Text, der gekürzt werden soll
+   * @param int $maxLength Maximal erlaubte Länge für den Text.
+   * @param string $moreTextIndicator Zeichenkette, die an das Ende eines gekürztes Textes gehängt werden soll
+   */
   public static function Truncate($text, $maxLength = 255, $moreTextIndicator = '...')
   {
     if (strlen($text) <= $maxLength)
@@ -55,7 +56,7 @@ class Strings
    */
   public static function Contains($haystack, $needle)
   {
-      return strpos($haystack, $needle) !== false;
+    return strpos($haystack, $needle) !== false;
   }
 
   /**
@@ -66,25 +67,25 @@ class Strings
    * @param boolean $caseInsensitive true, falls die Suche unabhängig von Groß-/Kleinschreibung arbeiten soll.
    * @return bool true, falls eine der Zeichenketten enthalten ist, sonst false.
    */
-  public static function ContainsAny($haystack, $needles, $caseInsensitive=false)
+  public static function ContainsAny($haystack, $needles, $caseInsensitive = false)
   {
-      if ($needles == null || count($needles) == 0)
-      {
-          return false;
-      }
+    if ($needles == null || count($needles) == 0)
+    {
+      return false;
+    }
 
     if ($caseInsensitive)
     {
-        $haystack = strtolower($haystack);
+      $haystack = strtolower($haystack);
     }
 
-    for ($i=0;$i<count($needles);$i++)
+    for ($i = 0; $i < count($needles); $i++)
     {
-        $currentNeedle = $caseInsensitive ? strtolower($needles[$i]) : $needles[$i];
-        if (self::Contains($haystack, $currentNeedle))
-        {
-            return true;
-        }
+      $currentNeedle = $caseInsensitive ? strtolower($needles[$i]) : $needles[$i];
+      if (self::Contains($haystack, $currentNeedle))
+      {
+        return true;
+      }
     }
 
     return false;
@@ -105,5 +106,47 @@ class Strings
 
     return is_numeric($text[0]);
   }
+
+  /**
+   * Führt eine Massenersetzung von Item-Geboten aus. Geht dabei davon aus, dass zwei gleich große Array mit
+   * Such-/Ersetzwerten angegeben werden. WICHTIG: bei zu vielen Ersetzung kann PHP einen Fehler werfen (und somit)
+   * das Ergebnis nicht richtig zurückgeben. In diesem Fall in kleineren Ersetzungs-Blöcken abarbeiten.
+   *
+   * @param string[] $search zu suchende Zeichenketten
+   * @param string[] $replace die Ersatz-Zeichenketten
+   * @param string $subject der Text, in dem die Ersetzungen durchgeführt werden sollen
+   * @param int|null $count falls gesetzt, enthält Count nach Abschluss die Anzahl der durchgeführten Ersetzungen
+   *
+   * @return 
+   */
+  public static function stringReplaceBulk($search, $replace, $subject, &$count = null)
+  {
+    $lookup = array_combine($search, $replace);
+    $result = preg_replace_callback(
+      '/' .
+        implode('|', array_map(
+          function ($s)
+          {
+            return preg_quote($s, '/');
+          },
+          $search
+        )) .
+        '/',
+      function ($matches) use ($lookup)
+      {
+        return $lookup[$matches[0]];
+      },
+      $subject,
+      -1,
+      $count
+    );
+
+    if ($result !== null || count($search) < 2)
+    {
+      return $result;
+    }
+
+    // falls ein Fehler aufgetreten ist, den Standardtext zurückgeben.
+    return $subject;
+  }
 }
-?>
